cb83f1c0b4aa91eafe8a0b6e805a2b4050e68b6c
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-06-12 16:08:54 +0530

    updated logo

--- client/src/layouts/Sidebar.jsx
+++ client/src/layouts/Sidebar.jsx
@@ -31,110 +31,19 @@ const Sidebar = () => {
     <>
       <div
         ref={sideBarRef}
-        className="sticky top-0 z-30 text-white bg-palette-chineseBlack lg:h-screen"
+        className="sticky top-0 z-30 lg:h-screen"
       >
         <div className="flex flex-row items-center justify-between w-full h-full p-5 lg:flex-col">
           <div className="flex items-center space-x-4 lg:space-x-0">
             <Link to="/">
-              <img className="w-10 h-auto" src={images.logo} alt="logo" />
+              <img className="h-auto bg-black w-14 " src={images.logo} alt="logo" />
             </Link>
             <div className="flex items-center lg:hidden">
               <UserProfileButton />
             </div>
           </div>
-          <div className="flex items-center space-x-4 lg:space-x-0">
-            <Cart className="lg:hidden" />
-            <div>
-              {!menuIsShown ? (
-                <AiOutlineMenu
-                  onClick={toggleMenuHandler}
-                  className="w-8 h-8 cursor-pointer"
-                />
-              ) : (
-                <AiOutlineClose
-                  onClick={toggleMenuHandler}
-                  className="w-8 h-8 cursor-pointer"
-                />
-              )}
-            </div>
-          </div>
-          <ul className="hidden space-y-2 lg:block">
-            <li>
-              <a
-                href="https://twitter.com/mmdrz003"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <AiOutlineTwitter className="w-5 h-5 hover:scale-110" />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/mohammadrz003"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <AiOutlineGithub className="w-5 h-5 hover:scale-110" />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.instagram.com/moonfo_dev/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <AiOutlineInstagram className="w-5 h-5 hover:scale-110" />
-              </a>
-            </li>
-          </ul>
         </div>
       </div>
-      {menuIsShown && (
-        <div
-          className="hidden lg:block z-[49] absolute bg-white opacity-60 top-0 bottom-0 right-0"
-          style={{ left: `${sideBarWidth}px` }}
-          onClick={toggleMenuHandler}
-        />
-      )}
-      {menuIsShown && (
-        <div
-          className={`hidden z-50 lg:flex items-center px-20 py-10 absolute top-0 bottom-0 w-1/3 h-full bg-palette-chineseBlack border-l border-l-[#202020]`}
-          style={{ left: `${sideBarWidth}px` }}
-        >
-          <ul className="space-y-5 text-4xl font-normal text-white uppercase">
-            <li className="tracking-wider">
-              <Link to="/">Shop</Link>
-            </li>
-
-            <li className="tracking-wider">
-              <Link to="/about">About</Link>
-            </li>
-
-            <li className="tracking-wider">
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </div>
-      )}
-      {menuIsShown && (
-        <div
-          className={`flex lg:hidden justify-center items-center p-5 fixed left-0 right-0 top-[80px] bottom-0 bg-palette-chineseBlack z-[21] border-t border-t-[#202020]`}
-        >
-          <ul className="space-y-5 text-3xl font-normal text-center text-white uppercase">
-            <li className="tracking-wider">
-              <Link to="/">Shop</Link>
-            </li>
-
-            <li className="tracking-wider">
-              <Link to="/about">About</Link>
-            </li>
-
-            <li className="tracking-wider">
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </div>
-      )}
     </>
   );
 };
